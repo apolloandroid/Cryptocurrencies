87ceb12824597023635ae19bc2d4329e81487d10
apolloandroid <hmelevskih94@gmail.com>
2020-09-02 00:30:11 +0300

    Added rateButton onClickListener

--- app/src/main/java/com/example/cryptocurrencies/AboutActivity.kt
+++ app/src/main/java/com/example/cryptocurrencies/AboutActivity.kt
@@ -1,12 +1,25 @@
 package com.example.cryptocurrencies
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        button_rate_application.setOnClickListener { rateApplication() }
+    }
+
+    private fun rateApplication() {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("market://details?id=${applicationContext.packageName}")
+        )
+        startActivity(intent)
     }
 }
