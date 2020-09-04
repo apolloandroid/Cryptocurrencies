b472cc8a29e16b93456f3a574f30b6e1dd8e2ae2
apolloandroid <hmelevskih94@gmail.com>
2020-09-04 23:57:35 +0300

    Did some refactoring

--- app/src/main/java/com/example/cryptocurrencies/ui/cryptocurrenciescurrencies/CryptocurrenciesFragment.kt
+++ app/src/main/java/com/example/cryptocurrencies/ui/cryptocurrenciescurrencies/CryptocurrenciesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrencies.ui.overview
+package com.example.cryptocurrencies.ui.cryptocurrenciescurrencies
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.example.cryptocurrencies.R
 
 
-class OverviewFragment : Fragment() {
+class CryptocurrenciesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +18,7 @@ class OverviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        return inflater.inflate(R.layout.fragment_cryptocurrencies, container, false)
     }
 
 }
