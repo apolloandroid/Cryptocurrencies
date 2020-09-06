50809ba79699fc9e91ce2ce273f25396408c8391
apolloandroid <hmelevskih94@gmail.com>
2020-09-06 21:26:53 +0300

    Did some refactoring

--- app/src/main/java/com/example/cryptocurrencies/ui/cryptocurrencies/CryptocurrenciesFragment.kt
+++ app/src/main/java/com/example/cryptocurrencies/ui/cryptocurrencies/CryptocurrenciesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrencies.ui.cryptocurrenciescurrencies
+package com.example.cryptocurrencies.ui.cryptocurrencies
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
